a50e91d4de09db3d7f6253921d3e494517d1101c
Lawrence Francell <lawrence@twintails.com>
2017-12-27 12:14:20 -0600

    Lobby Message length limit validation

---
+++ src/server/shared/validation/chat.js
@@ -0,0 +1,10 @@
+import { Validator } from './'
+
+export const MESSAGE_LENGTH_LIMIT = 240
+
+export function validateMessage(message) {
+  if(message.length > MESSAGE_LENGTH_LIMIT)
+    return Validator.fail('Message must be fewer than ${MESSAGE_LENGTH_LIMIT} characters long')
+
+  return Validator.succeed()
+}
