4ba153a5285c1dcba7d27c5ceb35f391ac3ee8a3
Lawrence Francell <lawrence@twintails.com>
2017-12-29 12:58:16 -0600

    stub game board components

---
+++ src/client/components/game/player-hand.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import "./player-hand.scss"
+
+import React from "react"
+import Card from "./card"
+
+export default function PlayerHand(props) {
+  const {hand, opSelectCard, selectCard, toggle,isOpen} = props
+  return <p>PLAYER HAND</p>
+}
