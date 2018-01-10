a0d58fa3cf5aa385d9d5b18f23724f9e4a0c31be
Lawrence Francell <lawrence@twintails.com>
2018-01-10 06:17:18 -0600

    Setup handlers

---
+++ src/server/lib/handlers.js
@@ -0,0 +1,19 @@
+export class HandlerBase {
+  constructor(client) {
+    this.client = client
+    this._onDisposes = []
+  }
+
+  onLogin() {
+
+  }
+
+  dispose() {
+    this._onDisposes.forEach(d => d())
+    this._onDisposes = []
+  }
+
+  onDispose(...callbacks) {
+    this._onDisposes.push(...callbacks)
+  }
+}
