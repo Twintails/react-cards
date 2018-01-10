75463a5e2b7a668831e1e32c3f6b6f160feb7bef
Lawrence Francell <lawrence@twintails.com>
2018-01-10 08:21:18 -0600

    STUB game and player models and server game actions

--- src/server/models/lobby.js
+++ src/server/models/lobby.js
@@ -1,6 +1,7 @@
 import _ from "lodash"
 import * as A from "../actions"
 import { RoomBase } from "../lib/room"
+import { Game } from "./game"
 
 export class Lobby extends RoomBase {
   get view() {
@@ -21,6 +22,11 @@ export class Lobby extends RoomBase {
     this.app = app
 
     this._nextGameId = 1
+
+    app.dispatcher.on({
+      [A.GAME_DISPOSED]: ({gameId}) => this.removeGame(gameId),
+      [A.GAME_SUMMARY_CHANGED]: () => this._tick()
+    })
   }
 
   sendMessage(client, message) {
@@ -34,5 +40,19 @@ export class Lobby extends RoomBase {
         message
       })
     })
+  }
+
+  createGame(title) {
+    const game = new Game(this._nextGameId++, title, this.app)
+    this._tick(() => this.game.push(game))
+    return game
+  }
+
+  removeGame(gameId) {
+    this._tick(() => _.remove(this.game, {id: gameId}))
+  }
+
+  getGameById(gameId) {
+    return _.find(this.game, {id: gameId})
   }
 }
---
+++ src/server/models/player.js
@@ -0,0 +1,50 @@
+import _ from "lodash"
+import * as A from "../actions"
+import { Validator } from "../shared/validation"
+import { RoomBase } from "../lib/room"
+
+export const HAND_SIZE = 10
+
+export class Player extends RoomBase {
+  get view() {
+    return {
+      id: this.id,
+      hand: this.hand.slice(),
+      stack: null
+    }
+  }
+
+  get summary() {
+    return {
+      id: this.id,
+      name: this.name,
+      score: this.score,
+      isCzar: false,
+      isPlaying: false,
+      isWinner: false
+    }
+  }
+
+  constructor(game, id, name) {
+    super(A.VIEW_PLAYER)
+    this.name = name
+    this.id = id
+    this.game = game
+    this.score = 0
+    this.hand = []
+    this._isDisposed = false
+  }
+
+  addPoints(points) {
+    this._tick(() => this.score += points)
+  }
+
+  dispose() {
+    if (this._isDisposed)
+      return
+
+    this.hand = []
+    this._isDisposed = true
+    this.game.app.dispatcher.emit(A.playerDisposed(this.game.id, this.id))
+  }
+}
