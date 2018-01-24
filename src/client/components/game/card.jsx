9abe9bf0b7e4b130905be0ebe27c7d60853a7512
Lawrence Francell <lawrence@twintails.com>
2018-01-24 09:19:17 -0600

    Now with double tap

--- src/client/components/game/card.jsx
+++ src/client/components/game/card.jsx
@@ -3,15 +3,60 @@ import "./card.scss"
 
 import React from "react"
 
+function hasClass(el, className) {
+  if (el.classList)
+    return el.classList.contains(className)
+  else
+    return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'))
+}
+
+function addClass(el, className) {
+  if (el.classList)
+    el.classList.add(className)
+  else if (!hasClass(el, className)) el.className += " " + className
+}
+
+function removeClass(el, className) {
+  if (el.classList)
+    el.classList.remove(className)
+  else if (hasClass(el, className)) {
+    var reg = new RegExp('(\\s|^)' + className + '(\\s|$)')
+    el.className=el.className.replace(reg, ' ')
+  }
+}
+
+function resetPlayableCards(elem) {
+  elem = elem.parentNode.firstChild;
+  do {
+    if (elem.nodeType === 3) continue; // text node
+    if (hasClass(elem, "is-playable")) {
+      removeClass(elem, "is-playable")
+    }
+  } while (elem = elem.nextSibling) // eslint-disable-line no-cond-assign
+  return;
+}
+
+function sibfilter(elem) {
+  switch (elem.nodeName.toUpperCase()) {
+    case 'DIV':
+      return true;
+    case 'SPAN':
+      return true;
+    default:
+      return false;
+  }
+}
+
 export default function Card(props) {
-  const {isSelectable = false, onClick, type, card, style = "default", canZoom = false} = props
+  const {isSelectable = false, isPlayable = false, onClick, type, card, style = "default", canZoom = false} = props
   const classes = [
     "c-card",
     type,
     card ? "front" : "back",
     `style-${style}`,
     canZoom ? "can-zoom" : "",
-    isSelectable ? "is-selectable" : ""
+    isSelectable ? "is-selectable" : "",
+    isPlayable ? "is-playable" : ""
   ]
 
   const click = (e) => {
@@ -22,7 +67,18 @@ export default function Card(props) {
 
   const tapp = (e) => {
     e.preventDefault();
+
+    let classes = e.currentTarget.classList
     if (!isSelectable) return
+    if (!isPlayable) {
+      if ( hasClass(e.currentTarget, "is-playable") ) {
+        return onClick(card)
+      }
+      resetPlayableCards(e.currentTarget)
+      addClass(e.currentTarget, "is-playable")
+      return
+    }
+
     onClick(card)
   }
 
